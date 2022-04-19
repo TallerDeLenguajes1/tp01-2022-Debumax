fc80a742f68220e7b6182cee5561747df71aac74
debora marcial <deboramarcial1991@gmail.com>
2022-04-19 00:58:09 -0300

    2e modificacion con parametros de referencia

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 
 //void invertir(int &a,int &b);
-void orden(int a , int b);
+void orden(int &a , int &b);
 int main(){
 	int a,b;
 	cout<<"ingrese el valor de a: ";
@@ -13,12 +13,8 @@ int main(){
 	//cout<<"valores de a y b respectivamente: "<<a<<" y "<<b;
 	orden( a , b);
 	
-
-
-	
 	return 0;
 }
-
 
 /*void invertir(int &a,int &b){
 	int ab;
@@ -28,17 +24,16 @@ int main(){
 	cout<<"valores de a y b respectivamente: "<<a<<" y "<<b;
 }*/
 
-void orden(int a , int b){
-	int min , max;
-	if(a<b){
-		min=a;
-		max=b;	
-		cout<<"min: "<<min<<"max: "<<max;	
+void orden(int &a , int &b){
+	int aux;
+	if(a<b){	
+		cout<<"min: "<<a<<"max: "<<b;	
 	}
 	if(a>b){
-		min=b;
-		max=a;
-		cout<<"min: "<<min<<"max: "<<max;
+		aux=a;
+		a=b;
+		b=aux;
+		cout<<"min: "<<a<<"max: "<<b;
 	}
 	if(a==b) cout<<"los valores son iguales";
 }
