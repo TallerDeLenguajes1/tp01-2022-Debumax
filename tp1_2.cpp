513f45253f4c4c06b5d38951afb889af80051f04
debora marcial <deboramarcial1991@gmail.com>
2022-04-19 00:54:18 -0300

    2e

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
 using namespace std;
 
-void invertir(int &a,int &b);
+//void invertir(int &a,int &b);
+void orden(int a , int b);
 int main(){
 	int a,b;
 	cout<<"ingrese el valor de a: ";
 	cin>>a;
 	cout<<"ingrese el valor de b: ";
 	cin>>b;
-	invertir(a,b);
-	cout<<"valores de a y b respectivamente: "<<a<<" y "<<b;
+	//invertir(a,b);
+	//cout<<"valores de a y b respectivamente: "<<a<<" y "<<b;
+	orden( a , b);
+	
 
 
 	
@@ -17,10 +20,25 @@ int main(){
 }
 
 
-void invertir(int &a,int &b){
+/*void invertir(int &a,int &b){
 	int ab;
 	ab=a;
 	a=b;
 	b=ab;
 	cout<<"valores de a y b respectivamente: "<<a<<" y "<<b;
+}*/
+
+void orden(int a , int b){
+	int min , max;
+	if(a<b){
+		min=a;
+		max=b;	
+		cout<<"min: "<<min<<"max: "<<max;	
+	}
+	if(a>b){
+		min=b;
+		max=a;
+		cout<<"min: "<<min<<"max: "<<max;
+	}
+	if(a==b) cout<<"los valores son iguales";
 }
