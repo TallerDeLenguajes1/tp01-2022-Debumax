7c7f7888bcfb02fb8c94061b316d7386d4662a00
debora marcial <deboramarcial1991@gmail.com>
2022-04-19 00:44:09 -0300

    2d - con parametros por referencia[C

--- tp1_2.cpp
+++ tp1_2.cpp
@@ -1,37 +1,26 @@
 #include <iostream>
 using namespace std;
-//float cuadrado(float a);
-//void cuadrado(float a);
-void muestra(float a );
+
+void invertir(int &a,int &b);
 int main(){
-	cout<<"hola mundo";
-	
-	int *p;
-	int numero=5;
-	p=&numero;
-	
-	/*cout<<"contenido del puntero: "<<*p<<"\n";
-	cout<<"direccion de memoria almacenada por el puntero: "<<p<<"\n";
-	cout<<"direccion de memoria almacenada por la variable: "<<&numero<<"\n";
-	cout<<"la direccion de memoria del puntero: "<<&p<<"\n";
-	cout<<"tamanio de memoria utilizado por la variable: "<<sizeof(numero)<<"\n";*/
-	
-	//cout<<"cuadrado de 5:  "<<cuadrado( numero);
-	//cuadrado(numero);
-	muestra(numero);
+	int a,b;
+	cout<<"ingrese el valor de a: ";
+	cin>>a;
+	cout<<"ingrese el valor de b: ";
+	cin>>b;
+	invertir(a,b);
+	cout<<"valores de a y b respectivamente: "<<a<<" y "<<b;
+
+
 	
 	return 0;
 }
 
-/*float cuadrado(float a){
-	
-	return a*a;
-}
-void cuadrado(float a){
-	cout<<"el cuadrado de 5 es: "<<a*a;
-}*/
-void muestra(float a ){
-	cout<<"direccion de la variable: "<<&a<<"\n";
-	cout<<"contenido de la variable: "<<a;
-}
 
+void invertir(int &a,int &b){
+	int ab;
+	ab=a;
+	a=b;
+	b=ab;
+	cout<<"valores de a y b respectivamente: "<<a<<" y "<<b;
+}
